718a6603be899f39430150d6445dce6e327f0fbf
mchopra-lockton <83672804+mchopra-lockton@users.noreply.github.com>
2021-06-16 18:13:05 -0400

    Update: Test.py notebooks

--- notebooks/Project/Nexsure/silver-gold-DIM_NX_COMM_TAX.py
+++ notebooks/Project/Nexsure/silver-gold-DIM_NX_COMM_TAX.py
@@ -3,7 +3,7 @@
 
 # COMMAND ----------
 
-# MAGIC %run "/Shared/Database Config"
+# MAGIC %run "/Project/Database Config"
 
 # COMMAND ----------
 
@@ -28,7 +28,7 @@ dbutils.widgets.removeAll()
 # Set the path for Silver layer for Nexsure
 
 now = datetime.now() 
-#sourceSilverPath = "Reference/Nexsure/DimDate/2021/05"
+#sourceSilverPath = "Reference/Nexsure/DimDate/2021/06"
 sourceSilverFolderPath = "Invoice/Nexsure/DimCommissionableTaxable/" +now.strftime("%Y") + "/" + now.strftime("%m")
 sourceSilverPath = SilverContainerPath + sourceSilverFolderPath
 
@@ -67,13 +67,13 @@ print (recordCountFilePath)
 # COMMAND ----------
 
 # Temporary cell - DELETE
-# now = datetime.now() 
-# GoldDimTableName = "Dim_NX_Comm_Tax"
-# GoldFactTableName = "FCT_NX_INV_LINE_ITEM_TRANS"
-# sourceSilverPath = "Invoice/Nexsure/DimCommissionableTaxable/" +now.strftime("%Y") + "/05"
-# sourceSilverPath = SilverContainerPath + sourceSilverPath
-# sourceSilverFile = "DimCommissionableTaxable_2021_05_21.parquet"
-# sourceSilverFilePath = sourceSilverPath + "/" + sourceSilverFile
+now = datetime.now() 
+GoldDimTableName = "Dim_NX_Comm_Tax"
+GoldFactTableName = "FCT_NX_INV_LINE_ITEM_TRANS"
+sourceSilverPath = "Invoice/Nexsure/DimCommissionableTaxable/" +now.strftime("%Y") + "/06"
+sourceSilverPath = SilverContainerPath + sourceSilverPath
+sourceSilverFile = "DimCommissionableTaxable_2021_06_04.parquet"
+sourceSilverFilePath = sourceSilverPath + "/" + sourceSilverFile
 # badRecordsPath = badRecordsRootPath + GoldDimTableName + "/"
 # recordCountFilePath = badRecordsPath + date_time + "/" + "RecordCount"
 # BatchId = "1afc2b6c-d987-48cc-ae8c-a7f41ea27249"
@@ -84,7 +84,7 @@ sourceSilverFilePath = "abfss://c360silver@dlsldpdev01v8nkg988.dfs.core.windows.
 
 # MAGIC %scala
 # MAGIC // Temporary cell - DELETE
-# MAGIC // lazy val GoldDimTableName = "Dim_NX_Comm_Tax"
+# MAGIC lazy val GoldDimTableName = "Dim_NX_Comm_Tax"
 
 # COMMAND ----------
 
@@ -118,9 +118,10 @@ sourceSilverDF.createOrReplaceTempView("DIM_NX_COMM_TAX")
 
 dummyDataDF = spark.sql(
   f"""
-SELECT -99999 as NX_COMM_TAX_KEY,
+SELECT  -99999 as NX_COMM_TAX_KEY,
       -1 as COMM_TAX_TYPE,
       -1 as TAXBLE_STATUS,
+      -1 as COMM_TAX_FLG,
       -1 as DB_SRC_KEY,
       -1 SRC_AUDT_KEY,
       '{ BatchId }' AS ETL_BATCH_ID,
@@ -137,6 +138,7 @@ f"""
 SELECT CommissionableTaxableKey as NX_COMM_TAX_KEY,
       CommissionableType as COMM_TAX_TYPE,
       TaxableStatus as TAXBLE_STATUS,
+      TaxableFlag as COMM_TAX_FLG,
       DBSourceKey as DB_SRC_KEY,
       AuditKey as SRC_AUDT_KEY,
       '{ BatchId }' AS ETL_BATCH_ID,
@@ -171,8 +173,8 @@ reconDF.write.jdbc(url=Url, table=reconTable, mode="append")
 # MAGIC // Truncate Fact table and Delete data from Dimension table
 # MAGIC lazy val connection = DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword)
 # MAGIC lazy val stmt = connection.createStatement()
-# MAGIC lazy val sql_truncate = "truncate table " + finalTableSchema + "." + "FCT_NX_INV_LINE_ITEM_TRANS"
-# MAGIC stmt.execute(sql_truncate)
+# MAGIC //lazy val sql_truncate = "truncate table " + finalTableSchema + "." + "FCT_NX_INV_LINE_ITEM_TRANS"
+# MAGIC //stmt.execute(sql_truncate)
 # MAGIC lazy val sql = "exec " + finalTableSchema + ".[DropAndCreateFKContraints] @GoldTableName = '" + GoldDimTableName + "'"
 # MAGIC stmt.execute(sql)
 # MAGIC connection.close()
