e4e22fd8ce6ddddf38966d19e0689897bb4974aa
mchopra-lockton <83672804+mchopra-lockton@users.noreply.github.com>
2021-06-16 18:17:21 -0400

    Update: Test.py notebooks

--- notebooks/Project/Nexsure/silver-gold-DIM_NX_ORG.py
+++ notebooks/Project/Nexsure/silver-gold-DIM_NX_ORG.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 # COMMAND ----------
 
-# MAGIC %run "/Shared/Database Config"
+# MAGIC %run "/Project/Database Config"
 
 # COMMAND ----------
 
@@ -70,14 +70,14 @@ print (recordCountFilePath)
 # now = datetime.now() 
 # GoldDimTableName = "DIM_NX_ORG"
 # GoldFactTableName = "FCT_NX_INV_LINE_ITEM_TRANS"
-# sourceSilverPath = "OrgStructure/Nexsure/DimOrgStructure/" +now.strftime("%Y") + "/05"
+# sourceSilverPath = "OrgStructure/Nexsure/DimOrgStructure/" +now.strftime("%Y") + "/06"
 # sourceSilverPath = SilverContainerPath + sourceSilverPath
-# sourceSilverFile = "DimOrgStructure_2021_05_21.parquet"
+# sourceSilverFile = "DimOrgStructure_2021_06_04.parquet"
 # sourceSilverFilePath = sourceSilverPath + "/" + sourceSilverFile
 # badRecordsPath = badRecordsRootPath + GoldDimTableName + "/"
 # recordCountFilePath = badRecordsPath + date_time + "/" + "RecordCount"
-# BatchId = "1afc2b6c-d987-48cc-ae8c-a7f41ea27249"
-# WorkFlowId ="8fc2895d-de32-4bf4-a531-82f0c6774221"
+#BatchId = "1afc2b6c-d987-48cc-ae8c-a7f41ea27249"
+#WorkFlowId ="8fc2895d-de32-4bf4-a531-82f0c6774221"
 sourceSilverFilePath = "abfss://c360silver@dlsldpdev01v8nkg988.dfs.core.windows.net/OrgStructure/Nexsure/DimOrgStructure/2021/06/DimOrgStructure_2021_06_04.parquet"
 
 # COMMAND ----------
@@ -99,7 +99,7 @@ spark.sql("set spark.sql.legacy.parquet.int96RebaseModeInRead=CORRECTED")
 try:
  
    sourceSilverDF = spark.read.parquet(sourceSilverFilePath)
-   #display(sourceSilverDF)
+   display(sourceSilverDF)
 except:
   # Log the error message
   errorDF = spark.createDataFrame([
@@ -122,6 +122,18 @@ f"""
 SELECT
 -99999 AS NX_ORG_KEY,
 -1 As DEPT_NO,
+-1 As BRNCH_NO,
+-1 As ORG_NO,
+-1 As DEPT_NAME,
+-1 As BRNCH_NAME,
+-1 As REGION_NAME,
+-1 As ORG_NAME,
+-1 As ENTY_LVL_NAME,
+-1 As SERIES,
+-1 As BUSS_UNIT,
+-1 As BUSS_TYP,
+-1 As DEPT_ALIAS,
+-1 As DEPT_MSTR_ALIAS,
 -1 as DB_SRC_KEY,
 -1 as SRC_AUDT_KEY,
 '{ BatchId }' AS ETL_BATCH_ID,
@@ -145,8 +157,14 @@ DepartmentName As DEPT_NAME,
 BranchNum As BRNCH_NO,
 BranchName As BRNCH_NAME,
 RegionName As REGION_NAME,
+OrgNumber As ORG_NO,
 OrgName	As ORG_NAME,
 EntityLevelName	As ENTY_LVL_NAME,
+-1 As SERIES,
+-1 As BUSS_UNIT,
+-1 As BUSS_TYP,
+-1 As DEPT_ALIAS,
+-1 As DEPT_MSTR_ALIAS,
 DBSourceKey As DB_SRC_KEY,
 AuditKey As SRC_AUDT_KEY,
 '{ BatchId }' AS ETL_BATCH_ID,
@@ -183,8 +201,8 @@ reconDF.write.jdbc(url=Url, table=reconTable, mode="append")
 # MAGIC // Truncate Fact table and Delete data from Dimension table
 # MAGIC lazy val connection = DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword)
 # MAGIC lazy val stmt = connection.createStatement()
-# MAGIC lazy val sql_truncate = "truncate table " + finalTableSchema + "." + "FCT_NX_INV_LINE_ITEM_TRANS"
-# MAGIC stmt.execute(sql_truncate)
+# MAGIC //lazy val sql_truncate = "truncate table " + finalTableSchema + "." + "FCT_NX_INV_LINE_ITEM_TRANS"
+# MAGIC //stmt.execute(sql_truncate)
 # MAGIC lazy val sql = "exec " + finalTableSchema + ".[DropAndCreateFKContraints] @GoldTableName = '" + GoldDimTableName + "'"
 # MAGIC stmt.execute(sql)
 # MAGIC connection.close()
