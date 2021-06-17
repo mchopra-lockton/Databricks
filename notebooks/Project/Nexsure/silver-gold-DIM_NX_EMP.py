db7d9a8641a5b77890bed3f8bc95ea4fc631930a
mchopra-lockton <83672804+mchopra-lockton@users.noreply.github.com>
2021-06-16 23:32:54 -0400

    Update: Test.py notebooks

--- notebooks/Project/Nexsure/silver-gold-DIM_NX_EMP.py
+++ notebooks/Project/Nexsure/silver-gold-DIM_NX_EMP.py
@@ -69,7 +69,7 @@ print (recordCountFilePath)
 # COMMAND ----------
 
 # Temporary cell to run manually - DELETE
-if (GoldFactTableName == "" or sourceSilverPath == "" or sourceSilverFile == ""):
+if (GoldDimTableName == "" or sourceSilverPath == "" or sourceSilverFile == ""):
   now = datetime.now() 
   GoldDimTableName = "DIM_NX_EMP"
   GoldFactTableName = "FCT_NX_INV_LINE_ITEM_TRANS"
@@ -87,7 +87,7 @@ if (GoldFactTableName == "" or sourceSilverPath == "" or sourceSilverFile == "")
 
 # MAGIC %scala
 # MAGIC // Temporary cell to run manually - DELETE
-# MAGIC if (GoldFactTableName == "") {
+# MAGIC if (GoldDimTableName == "") {
 # MAGIC  lazy val GoldDimTableName = "Dim_NX_Emp"
 # MAGIC }  
 
