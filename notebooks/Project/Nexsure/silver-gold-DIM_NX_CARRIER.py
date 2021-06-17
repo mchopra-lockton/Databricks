e611930ff93cb90def7c9b2c8631940f1e47df93
mchopra-lockton <83672804+mchopra-lockton@users.noreply.github.com>
2021-06-16 23:09:18 -0400

    Update: Test.py notebooks

--- notebooks/Project/Nexsure/silver-gold-DIM_NX_CARRIER.py
+++ notebooks/Project/Nexsure/silver-gold-DIM_NX_CARRIER.py
@@ -63,25 +63,28 @@ print (recordCountFilePath)
 
 # COMMAND ----------
 
-# Temporary cell - DELETE
-# now = datetime.now() 
-# GoldDimTableName = "Dim_NX_Carrier"
-# GoldFactTableName = "FCT_NX_INV_LINE_ITEM_TRANS"
-# sourceSilverPath = "Client/Nexsure/DimEntity/" +now.strftime("%Y") + "/05"
-# sourceSilverPath = SilverContainerPath + sourceSilverPath
-# sourceSilverFile = "DimEntity_2021_05_25.parquet"
-# sourceSilverFilePath = sourceSilverPath + "/" + sourceSilverFile
-# badRecordsPath = badRecordsRootPath + GoldDimTableName + "/"
-# recordCountFilePath = badRecordsPath + date_time + "/" + "RecordCount"
-# BatchId = "1afc2b6c-d987-48cc-ae8c-a7f41ea27249"
-# WorkFlowId ="8fc2895d-de32-4bf4-a531-82f0c6774221"
-sourceSilverFilePath = "abfss://c360silver@dlsldpdev01v8nkg988.dfs.core.windows.net/Client/Nexsure/DimEntity/2021/06/DimEntity_2021_06_04.parquet"
+# Temporary cell to run manually - DELETE
+if (GoldFactTableName == "" or sourceSilverPath == "" or sourceSilverFile == ""):
+  now = datetime.now() 
+  GoldDimTableName = "Dim_NX_Carrier"
+  GoldFactTableName = "FCT_NX_INV_LINE_ITEM_TRANS"
+  sourceSilverPath = "Client/Nexsure/DimEntity/" +now.strftime("%Y") + "/05"
+  sourceSilverPath = SilverContainerPath + sourceSilverPath
+  sourceSilverFile = "DimEntity_2021_05_25.parquet"
+  sourceSilverFilePath = sourceSilverPath + "/" + sourceSilverFile
+  badRecordsPath = badRecordsRootPath + GoldDimTableName + "/"
+  recordCountFilePath = badRecordsPath + date_time + "/" + "RecordCount"
+  BatchId = "1afc2b6c-d987-48cc-ae8c-a7f41ea27249"
+  WorkFlowId ="8fc2895d-de32-4bf4-a531-82f0c6774221"
+  sourceSilverFilePath = "abfss://c360silver@dlsldpdev01v8nkg988.dfs.core.windows.net/Client/Nexsure/DimEntity/2021/06/DimEntity_2021_06_04.parquet"
 
 # COMMAND ----------
 
 # MAGIC %scala
-# MAGIC // Temporary cell - DELETE
+# MAGIC // Temporary cell to run manually - DELETE
+# MAGIC if (GoldFactTableName == "") {
 # MAGIC // lazy val GoldDimTableName = "Dim_NX_Carrier"
+# MAGIC }  
 
 # COMMAND ----------
 
