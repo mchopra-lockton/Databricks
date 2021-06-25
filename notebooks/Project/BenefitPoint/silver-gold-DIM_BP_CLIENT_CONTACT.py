ea1447ccaab25aa930166bc0ea9a0f2b935564a4
mchopra-lockton <83672804+mchopra-lockton@users.noreply.github.com>
2021-06-25 18:45:04 -0400

    Update: Test.py notebooks

---
+++ notebooks/Project/BenefitPoint/silver-gold-DIM_BP_CLIENT_CONTACT.py
@@ -0,0 +1,228 @@
+# Databricks notebook source
+from datetime import datetime
+
+# COMMAND ----------
+
+# MAGIC %run "/Project/Database Config"
+
+# COMMAND ----------
+
+spark.conf.set(
+  ADLSConnectionURI,
+  ADLSConnectionKey
+)
+
+# COMMAND ----------
+
+dbutils.widgets.removeAll()
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC 
+# MAGIC dbutils.widgets.text("TableName", "","")
+# MAGIC lazy val GoldDimTableName = dbutils.widgets.get("TableName")
+# MAGIC 
+# MAGIC lazy val GoldFactTableName = "Gold.FCT_BP_INV_LINE_ITEM_TRANS"
+# MAGIC print (GoldDimTableName)
+# MAGIC print (GoldFactTableName)
+
+# COMMAND ----------
+
+# Set the path for Silver layer for Nexsure
+
+now = datetime.now() 
+
+sourceSilverFilePath = SilverContainerPath + "Client/Benefits/vw_CLIENT_CONTACT_AllRecs/" +now.strftime("%Y") + "/" + now.strftime("%m") + "/" + "vw_CLIENT_CONTACT_AllRecs_" + now.strftime("%Y") + "_" + now.strftime("%m") + "_" + now.strftime("%d") + ".parquet"
+
+dbutils.widgets.text("TableName", "","")
+GoldDimTableName = dbutils.widgets.get("TableName")
+
+dbutils.widgets.text("BatchId", "","")
+BatchId = dbutils.widgets.get("BatchId")
+
+dbutils.widgets.text("WorkFlowId", "","")
+WorkFlowId = dbutils.widgets.get("WorkFlowId")
+
+#Set the file path to log error
+badRecordsPath = badRecordsRootPath + GoldDimTableName + "/"
+
+
+now = datetime.now() # current date and time
+date_time = now.strftime("%Y%m%dT%H%M%S")
+badRecordsFilePath = badRecordsPath + date_time + "/" + "ErrorRecords"
+recordCountFilePath = badRecordsPath + date_time + "/" + "RecordCount"
+
+#Set the file path to log error
+#badRecordsPath = badRecordsRootPath + "/" + sourceTable + "/"
+
+print ("Param -\'Variables':")
+print (sourceSilverFilePath)
+print (badRecordsFilePath)
+print (recordCountFilePath)
+
+# COMMAND ----------
+
+# Temporary cell - DELETE
+now = datetime.now() 
+GoldDimTableName = "DIM_BP_CLIENT_CONTACT"
+badRecordsPath = badRecordsRootPath + GoldDimTableName + "/"
+recordCountFilePath = badRecordsPath + date_time + "/" + "RecordCount"
+BatchId = "1afc2b6c-d987-48cc-ae8c-a7f41ea27249"
+WorkFlowId ="8fc2895d-de32-4bf4-a531-82f0c6774221"
+sourceSilverFilePath = "abfss://c360silver@dlsldpdev01v8nkg988.dfs.core.windows.net/Client/Benefits/vw_CLIENT_CONTACT_AllRecs/" + yymmManual + "/vw_CLIENT_CONTACT_AllRecs_" + yyyymmddManual + ".parquet"
+print(sourceSilverFilePath)
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC // Temporary cell - DELETE
+# MAGIC  val GoldDimTableName = "DIM_BP_CLIENT_CONTACT"
+
+# COMMAND ----------
+
+# Do not proceed if any of the parameters are missing
+if (GoldDimTableName == "" or sourceSilverPath == ""):
+  dbutils.notebook.exit({"exceptVariables": {"errorCode": {"value": "Input parameters are missing"}}})
+
+# COMMAND ----------
+
+# Read source file
+spark.sql("set spark.sql.legacy.parquet.int96RebaseModeInRead=CORRECTED")
+try:
+ 
+  sourceSilverDF = spark.read.parquet(sourceSilverFilePath)
+  #display(sourceSilverDF)
+except:
+  # Log the error message
+  errorDF = spark.createDataFrame([
+    (GoldDimTableName,now,sourceSilverFilePath,BatchId,WorkFlowId,"Error reading the file")
+  ],["TableName","ETL_CREATED_DT","Filename","ETL_BATCH_ID","ETL_WRKFLW_ID","Message"])
+  # Write the recon record to SQL DB
+  errorDF.write.jdbc(url=Url, table=reconTable, mode="append")  
+  dbutils.notebook.exit({"exceptVariables": {"errorCode": {"value": "Error reading the file: " + sourceSilverFilePath}}}) 
+
+# COMMAND ----------
+
+sourceSilverDF.createOrReplaceTempView("DIM_BP_CLIENT_CONTACT")
+
+# COMMAND ----------
+
+pushdown_query = "(select CLIENT_ID, from [dbo].[DIM_BP_CLIENT]) PC"
+carrierDF = spark.read.jdbc(url=Url, table=pushdown_query, properties=connectionProperties)
+display(carrierDF)
+# Register table so it is accessible via SQL Context
+carrierDF.createOrReplaceTempView("DIM_BP_CLIENT")
+
+# COMMAND ----------
+
+dummyDataDF = spark.sql(
+f""" 
+SELECT
+-99999 as BP_CLNT_CONTCT_ID,
+-99999 as CLNT_ID,
+-1 as FULL_NAME,
+-1 as TITLE,
+-1 as EMAIL_ADDR,
+-1 as ADDR_LINE_1,
+-1 as ADDR_LINE_2,
+-1 as CITY,
+-1 as STATE,
+-1 as ZIP,
+0 as PRIM_CONTCT_IND,
+-1 as WRK_AREA_CD,
+-1 as WRK_AREA_PHN_NO,
+'1800-01-01' as BP_LAST_MODIFIED_DATE,
+'1800-01-01' as SRC_ROW_BEGN_DT,
+'1800-01-01' as SRC_ROW_END_DT,
+0 As SURR_CLNT_ID
+'{ BatchId }' AS ETL_BATCH_ID,
+'{ WorkFlowId }' AS ETL_WRKFLW_ID,
+current_timestamp() AS ETL_CREATED_DT,
+current_timestamp() AS ETL_UPDATED_DT
+FROM DIM_BP_CLIENT_CONTACT limit 1
+"""
+)
+#display(dummyDataDF)
+
+# COMMAND ----------
+
+# Get final set of records
+finalDataDF = spark.sql(
+f""" 
+SELECT
+CLIENT_CONTACT_ID as BP_CLNT_CONTCT_ID,
+CLIENT_ID as CLNT_ID,
+'' as FULL_NAME,
+'' as TITLE,
+'' as EMAIL_ADDR,
+'' as ADDR_LINE_1,
+'' as ADDR_LINE_2,
+'' as CITY,
+'' as STATE,
+'' as ZIP,
+PRIMARY_CONTACT_IND as PRIM_CONTCT_IND,
+'' as WRK_AREA_CD,
+'' as WRK_AREA_PHN_NO,
+'1800-01-01' as BP_LAST_MODIFIED_DATE,
+RowBeginDate as SRC_ROW_BEGN_DT,
+RowEndDate as SRC_ROW_END_DT,
+coalesce(client.SURR_CLNT_ID,0) As SURR_CLNT_ID,
+'{ BatchId }' AS ETL_BATCH_ID,
+'{ WorkFlowId }' AS ETL_WRKFLW_ID,
+current_timestamp() AS ETL_CREATED_DT,
+current_timestamp() AS ETL_UPDATED_DT
+FROM DIM_BP_CLIENT_CONTACT cc
+JOIN DIM_BP_CLIENT client on cc.CLIENT_ID = client.CLNT_ID
+"""
+)
+#display(finalDataDF)
+
+# COMMAND ----------
+
+# Do not proceed if there are no records to insert
+if (finalDataDF.count() == 0):
+  dbutils.notebook.exit({"exceptVariables": {"errorCode": {"value": "There are no records to insert: " + sourceSilverFilePath}}})
+
+# COMMAND ----------
+
+# Create a dataframe for record count
+sourceRecordCount = sourceSilverDF.count()
+targetRecordCount = finalDataDF.count()
+#errorRecordCount = errorDataDF.count()
+reconDF = spark.createDataFrame([
+    (GoldDimTableName,now,sourceRecordCount,targetRecordCount,sourceSilverFilePath,BatchId,WorkFlowId)
+  ],["TableName","ETL_CREATED_DT","SourceRecordCount","TargetRecordCount","Filename","ETL_BATCH_ID","ETL_WRKFLW_ID"])
+
+# Write the recon record to SQL DB
+reconDF.write.jdbc(url=Url, table=reconTable, mode="append")
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC // Truncate Fact table and Delete data from Dimension table
+# MAGIC lazy val connection = DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword)
+# MAGIC lazy val stmt = connection.createStatement()
+# MAGIC //lazy val sql_truncate = "truncate table " + GoldFactTableName
+# MAGIC //stmt.execute(sql_truncate)
+# MAGIC lazy val sql = "exec " + finalTableSchema + ".[DropAndCreateFKContraints] @GoldTableName = '" + GoldDimTableName + "'"
+# MAGIC stmt.execute(sql)
+# MAGIC connection.close()
+
+# COMMAND ----------
+
+GoldDimTableNameComplete = finalTableSchema + "." + GoldDimTableName
+dummyDataDF.write.jdbc(url=Url, table=GoldDimTableNameComplete, mode="append")
+finalDataDF.write.jdbc(url=Url, table=GoldDimTableNameComplete, mode="append")
+
+# COMMAND ----------
+
+# Write the final parquet file to Gold zone
+dbutils.widgets.text("ProjectFolderName", "","")
+sourceGoldPath = dbutils.widgets.get("ProjectFolderName")
+dbutils.widgets.text("ProjectFileName", "","")
+sourceGoldFile = dbutils.widgets.get("ProjectFileName")
+
+spark.sql("set spark.sql.legacy.parquet.int96RebaseModeInWrite=CORRECTED")
+sourceGoldFilePath = GoldContainerPath + sourceGoldPath + "/" + sourceGoldFile
+finalDataDF.write.mode("overwrite").parquet(sourceGoldFilePath)
