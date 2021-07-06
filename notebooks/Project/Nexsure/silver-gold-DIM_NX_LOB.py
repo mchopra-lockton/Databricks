d3877b62d3edac47ea7ac651c9f8d88cc76e0fed
mchopra-lockton <83672804+mchopra-lockton@users.noreply.github.com>
2021-07-05 22:38:45 -0400

    Update: Test.py notebooks

--- notebooks/Project/Nexsure/silver-gold-DIM_NX_LOB.py
+++ notebooks/Project/Nexsure/silver-gold-DIM_NX_LOB.py
@@ -23,7 +23,12 @@ dbutils.widgets.removeAll()
 # MAGIC %scala
 # MAGIC 
 # MAGIC dbutils.widgets.text("TableName", "","")
-# MAGIC lazy val GoldDimTableName = dbutils.widgets.get("TableName")
+# MAGIC var GoldDimTableName = dbutils.widgets.get("TableName")
+# MAGIC 
+# MAGIC // USE WHEN RUN IN DEBUG MODE
+# MAGIC if (RunInDebugMode != "No") {
+# MAGIC   GoldDimTableName = "Dim_NX_LOB"
+# MAGIC }
 
 # COMMAND ----------
 
@@ -69,34 +74,16 @@ print (recordCountFilePath)
 
 # COMMAND ----------
 
-# Temporary cell to run manually - DELETE
-if (GoldDimTableName == "" or sourceSilverPath == "" or sourceSilverFile == ""):
+# USE WHEN RUN IN DEBUG MODE
+if (RunInDebugMode != 'No'):
   now = datetime.now() 
   GoldDimTableName = "DIM_NX_LOB"
   GoldFactTableName = "FCT_NX_INV_LINE_ITEM_TRANS"
-  sourceSilverPath = "Invoice/Nexsure/DimLineItemLoB/" +now.strftime("%Y") + "/06"
-  sourceSilverPath = SilverContainerPath + sourceSilverPath
-  sourceSilverFile = "DimLineItemLoB_2021_06_04.parquet"
-  sourceSilverFilePath = sourceSilverPath + "/" + sourceSilverFile
   badRecordsPath = badRecordsRootPath + GoldDimTableName + "/"
   recordCountFilePath = badRecordsPath + date_time + "/" + "RecordCount"
   BatchId = "1afc2b6c-d987-48cc-ae8c-a7f41ea27249"
   WorkFlowId ="8fc2895d-de32-4bf4-a531-82f0c6774221"
-  sourceSilverFilePath = "abfss://c360silver@dlsldpdev01v8nkg988.dfs.core.windows.net/Invoice/Nexsure/DimLineItemLoB/2021/06/DimLineItemLoB_2021_06_04.parquet"
-
-# COMMAND ----------
-
-# MAGIC %scala
-# MAGIC // Temporary cell to run manually - DELETE
-# MAGIC if (GoldDimTableName == "") {
-# MAGIC   val GoldDimTableName = "Dim_NX_LOB"
-# MAGIC }
-
-# COMMAND ----------
-
- # Do not proceed if any of the parameters are missing
-if (GoldDimTableName == "" or sourceSilverPath == "" or sourceSilverFile == ""):
-  dbutils.notebook.exit({"exceptVariables": {"errorCode": {"value": "Input parameters are missing"}}})
+  sourceSilverFilePath = "abfss://c360silver@dlsldpdev01v8nkg988.dfs.core.windows.net/Invoice/Nexsure/DimLineItemLoB/" + yymmManual + "/DimLineItemLoB_" + yyyymmddManual + ".parquet"
 
 # COMMAND ----------
 
@@ -113,7 +100,7 @@ except:
   ],["TableName","ETL_CREATED_DT","Filename","ETL_BATCH_ID","ETL_WRKFLW_ID","Message"])
   # Write the recon record to SQL DB
   errorDF.write.jdbc(url=Url, table=reconTable, mode="append")  
-  dbutils.notebook.exit({"exceptVariables": {"errorCode": {"value": "Error reading the file: " + sourceSilverFilePath}}})  
+  #dbutils.notebook.exit({"exceptVariables": {"errorCode": {"value": "Error reading the file: " + sourceSilverFilePath}}})  
 
 # COMMAND ----------
 
@@ -182,18 +169,6 @@ reconDF = spark.createDataFrame([
 
 # Write the recon record to SQL DB
 reconDF.write.jdbc(url=Url, table=reconTable, mode="append")
-
-# COMMAND ----------
-
-# MAGIC %scala
-# MAGIC // Truncate Fact table and Delete data from Dimension table
-# MAGIC lazy val connection = DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword)
-# MAGIC lazy val stmt = connection.createStatement()
-# MAGIC //lazy val sql_truncate = "truncate table " + finalTableSchema + "." + "FCT_NX_INV_LINE_ITEM_TRANS"
-# MAGIC //stmt.execute(sql_truncate)
-# MAGIC lazy val sql = "exec " + finalTableSchema + ".[DropAndCreateFKContraints] @GoldTableName = '" + GoldDimTableName + "'"
-# MAGIC stmt.execute(sql)
-# MAGIC connection.close()
 
 # COMMAND ----------
 
